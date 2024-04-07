2e26df0640bb612fd62df27190efeccf0bc7b483
Mike Ledger <eleventynine@gmail.com>
2024-04-08 04:23:47 +1000

    images

--- lib/Nice/Terminal/Render.hs
+++ lib/Nice/Terminal/Render.hs
@@ -1,84 +1,97 @@
-{-# LANUAGE MagicHash #-}
+{-# LANGUAGE Strict #-}
 {-# LANGUAGE NoFieldSelectors #-}
 
 module Nice.Terminal.Render where
 
-import Data.Array.IO as Arr
+import Control.Monad.ST.Strict
+import Data.Array.ST as Arr
 import Data.Array.Unboxed
-import Data.Ix
 import Prelude hiding (State)
 
-type Buf = IOUArray (V2 Int) Char
-
-newtype Env = Env
-  { printBuf :: Buf -> Render ()
+data Extent s = Extent
+  { backing :: STUArray s (V2 Int) Char
+  , size :: V2 Int
   }
 
-data State = State
-  { offsetY :: !Int
-  , buf :: !Buf
-  , maxCoord :: !(V2 Int)
-  }
+modifyExtent :: V2 Int -> Extent s -> (Char -> Char) -> ST s (Extent s)
+modifyExtent coord@(V2 x y) Extent {backing = backing0, size = size@(V2 w h)} modify = do
+  cur <- readArray backing0 coord
+  writeArray backing0 coord $! modify cur
+  let sx = if x > w then 2 else 1
+  let sy = if y > h then 2 else 1
+  next <- newArray (0, V2 (sx * max x w) (sy * max y h)) ' '
+  forM_ (range (0, size)) \i -> do
+    orig <- readArray next i
+    writeArray next i orig
+  pure Extent {backing = next, size = max size coord}
 
-newtype Render a = Render
-  { unRender :: Env -> State -> IO (a, State)
-  }
-  deriving (Functor, Applicative, Monad, MonadState State, MonadReader Env) via (ReaderT Env (StateT State IO))
+-- --------------------------------------------------------------------------------
 
-instance MonadIO Render where
-  liftIO m = Render \_ s -> (,s) <$> m
+-- newtype ImageBuf = ImageBuf (forall s. Extent s -> ST s (Extent s))
 
-print2dArray :: IOUArray (V2 Int) Char -> Render ()
-print2dArray arr = do
-  State {maxCoord = V2 w h, offsetY} <- get
-  liftIO $ forIntRange 0 ((h + 1) - offsetY) \y -> do
-    evaluatingStateT ([] :: [Char]) do
-      forIntRange 0 (w + 1) \x -> do
-        ch <- liftIO (readArray arr (V2 x y))
-        modify' (ch :)
-      str <- gets (reverse . dropWhile (== ' '))
-      putStrLn str
+-- instance Semigroup ImageBuf where
+--   ImageBuf a <> ImageBuf b = ImageBuf (a >=> b)
 
-runRender :: Render a -> IO a
-runRender (Render f) = do
-  buf <- newArray (0, V2 60 60) ' '
-  (a, _) <- f (Env print2dArray) State {offsetY = 0, buf, maxCoord = -1}
-  pure a
+-- instance Monoid ImageBuf where
+--   mempty = ImageBuf pure
 
--- | Pop the current buffer and create a new one with the same size as the old
--- Call this regularly to avoid excessively large buffers, but keep in mind that
--- once the buffer is popped, those characters can no longer be written to
-popBuf :: Render ()
-popBuf = do
-  Env {printBuf} <- ask
-  State {maxCoord = V2 _ mh, buf} <- get
-  printBuf buf
-  (_upperLeft, V2 w h) <- liftIO (getBounds buf) -- XXX upperLeft must be (0, 0)
-  next <- liftIO (newArray (0, V2 w h) ' ')
-  put State {offsetY = mh + 1, buf = next, maxCoord = -1}
+-- char :: V2 Int -> (Char -> Char) -> ImageBuf
+-- char coord modifyChar =
+--   fitImageBuf coord <> ImageBuf \buf -> do
+--     cur <- readArray buf coord
+--     writeArray buf coord $! modifyChar cur
+--     pure buf
 
-fitBuf :: V2 Int -> Render ()
-fitBuf fin@(V2 x y) = do
-  State {offsetY, buf, maxCoord} <- get
-  bounds@(_, V2 w h) <- liftIO (getBounds buf)
-  unless (inRange bounds fin) do
-    let !sx = if x > w then 2 else 1
-    let !sy = if y > h then 2 else 1
-    next <- liftIO (newArray (0, V2 (sx * max x w) (sy * max y h)) ' ')
-    liftIO $ forM_ (range bounds) \i -> do
-      orig <- readArray buf i
-      writeArray next i orig
-    put State {offsetY, buf = next, maxCoord}
+-- chars :: V2 Int -> [Char -> Char] -> ImageBuf
+-- chars coord@(V2 x y) str =
+--   fitImageBuf (coord + V2 (x + length str) y) <> ImageBuf \buf -> do
+--     iforM_ str \i modifyChar -> do
+--       cur <- readArray buf (V2 (x + i) y)
+--       writeArray buf coord $! modifyChar cur
+--     pure buf
 
-modifyBuf :: V2 Int -> (Char -> Char) -> Render ()
-modifyBuf p@(V2 x0 y0) fn = do
-  State {offsetY, buf, maxCoord} <- get
-  put State {offsetY, buf, maxCoord = max maxCoord p}
-  bounds <- liftIO (getBounds buf) -- XXX upperLeft must be (0, 0)
-  when (y0 < offsetY) do
-    error "Nice.Terminal.Render.write: Tried to write to a region that was already popped with popBuf"
-  let fin = V2 x0 (y0 - offsetY)
-  unless (inRange bounds fin) (fitBuf fin)
-  liftIO do
-    ex <- readArray buf fin
-    writeArray buf fin $! fn ex
+-- str :: V2 Int -> String -> ImageBuf
+-- str coord@(V2 x y) str =
+--   fitImageBuf (coord + V2 (x + length str) y) <> ImageBuf \buf -> do
+--     iforM_ str \i -> writeArray buf (V2 (x + i) y)
+--     pure buf
+
+-- --------------------------------------------------------------------------------
+
+-- fitImageBuf :: V2 Int -> ImageBuf
+-- fitImageBuf coord@(V2 x y) = ImageBuf \buf -> do
+--   bounds@(_zeros, V2 w h) <- getBounds buf
+--   if inRange bounds coord
+--     then pure buf
+--     else do
+--       let sx = if x > w then 2 else 1
+--       let sy = if y > h then 2 else 1
+--       next <- newArray (0, V2 (sx * max x w) (sy * max y h)) ' '
+--       forM_ (range bounds) \i -> do
+--         orig <- readArray buf i
+--         writeArray next i orig
+--       pure next
+
+-- --------------------------------------------------------------------------------
+
+-- newtype Image = Image (UArray (V2 Int) Char)
+
+-- renderImageBuf :: ImageBuf -> Image
+-- renderImageBuf (ImageBuf writer) = Image $ runST do
+--   buf0 <- newArray (V2 0 0, V2 0 0) ' '
+--   final <- writer buf0
+--   freeze final
+
+-- putImageLn :: Image -> IO ()
+-- putImageLn (Image buf) = do
+--   let (_zeros, V2 w h) = bounds buf
+--   forIntRange 0 (h + 1) \y -> do
+--     str <- evaluatingStateT ([] :: [Char]) do
+--       forIntRange 0 (w + 1) \x -> do
+--         let ch = buf ! V2 x y
+--         modify' (ch :)
+--       gets (reverse . dropWhile (== ' '))
+--     putStrLn str
+
+-- imageSize :: Image -> V2 Int
+-- imageSize (Image img) = snd (bounds img)
